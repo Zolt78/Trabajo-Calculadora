fc71a75cc398c7e2d4262ee25abf0471b1f092bc
UserAI678 <138083898+UserAI678@users.noreply.github.com>
2025-06-14 02:22:24 -0300

    Añadiendo la funcion de poner y quitar números

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 const screen = document.querySelector(".screen");
+
+const Botones = document.querySelectorAll(".btn");
+
+Botones.forEach((boton) => {
+    boton.addEventListener("click", ()=>{
+        const botonApretado = boton.textContent;
+
+        if(boton.id === "c"){
+            screen.textContent = "0";
+            return;
+        }
+
+        if(boton.id === "delete"){
+            if(screen.textContent.length === 1){
+                screen.textContent = "0";
+        
+            }else{
+                screen.textContent = screen.textContent.slice(0,-1);
+            }
+            return;
+        }
+
+
+
+        if (screen.textContent === "0") {
+            screen.textContent = botonApretado;
+
+        }else {
+            screen.textContent += botonApretado;
+        }
+
+ 
+    })
+})
+
+
+
